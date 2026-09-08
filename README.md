04a3e638271c45725263ff51a2f6aea00f9bbf21
Giovana <giovananunesdasilvar389182@gmail.com>
2026-09-07 22:00:12 -0300

    Commitando as primeiras alterações

--- README.md
+++
@@ -1,2 +0,0 @@
-# blackjack
-Jogo blackjack feito em Python e adicionando futuramente HTML, CSS e JS.
--- blackjack/main.py
+++ blackjack/main.py
@@ -1,2 +1,16 @@
-print("blackjack")
-print("----Bem-vindo ao jogo de Blackjack!----")
+print("----Bem-vindo ao jogo de Blackjack!----\n")
+
+print("Regras do jogo:\n")
+print("1. O objetivo é ter um valor de carta mais próximo de 21 possível, sem exceder.")
+print("2. As cartas numéricas valem o valor indicado.")
+print("3. As cartas figura (Valete, Dama, Rei) valem 10.")
+print("4. O Ás pode valer 1 ou 11, dependendo do contexto.")
+print("5. Se o jogador ultrapassar 21, ele perde.")
+print("6. Se o dealer ultrapassar 21, o jogador ganha.")
+print("7. Se o jogador tiver um valor mais alto que o dealer sem ultrapassar 21, ele ganha.")
+print("8. Se o dealer e o jogador tiverem o mesmo valor, é um empate.")
+
+cartas = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+cartas_valores = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 1 or 11}
+
+
